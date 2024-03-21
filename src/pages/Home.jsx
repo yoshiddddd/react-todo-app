2998670b829e9d724f374cf57e2255844a3fa2fe
yoshiddddd <yoshidakerapo@gmail.com>
2024-03-21 20:27:51 +0900

    日付昨日の実装:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import { Header } from "../components/Header";
 import { url } from "../const";
-import "./home.css";
+import "./home.scss";
+import moment from 'moment';
+import 'moment/locale/ja'; // 日本語ロケールをインポート
 
-
+moment.locale('ja');
 export const Home = () => {
   const [isDoneDisplay, setIsDoneDisplay] = useState("todo"); // todo->未完了 done->完了
   const [lists, setLists] = useState([]);
@@ -16,51 +18,54 @@ export const Home = () => {
   const [cookies] = useCookies();
   const handleIsDoneDisplayChange = (e) => setIsDoneDisplay(e.target.value);
   useEffect(() => {
-    axios.get(`${url}/lists`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      setLists(res.data)
-    })
-    .catch((err) => {
-      setErrorMessage(`リストの取得に失敗しました。${err}`);
-    })
+    axios
+      .get(`${url}/lists`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        setLists(res.data);
+      })
+      .catch((err) => {
+        setErrorMessage(`リストの取得に失敗しました。${err}`);
+      });
   }, []);
 
   useEffect(() => {
-    const listId = lists[0]?.id
-    if(typeof listId !== "undefined"){
-      setSelectListId(listId)
-      axios.get(`${url}/lists/${listId}/tasks`, {
-        headers: {
-          authorization: `Bearer ${cookies.token}`
-        }
-      })
-      .then((res) => {
-        setTasks(res.data.tasks)
-      })
-      .catch((err) => {
-        setErrorMessage(`タスクの取得に失敗しました。${err}`);
-      })
+    const listId = lists[0]?.id;
+    if (typeof listId !== "undefined") {
+      setSelectListId(listId);
+      axios
+        .get(`${url}/lists/${listId}/tasks`, {
+          headers: {
+            authorization: `Bearer ${cookies.token}`,
+          },
+        })
+        .then((res) => {
+          setTasks(res.data.tasks);
+        })
+        .catch((err) => {
+          setErrorMessage(`タスクの取得に失敗しました。${err}`);
+        });
     }
   }, [lists]);
 
   const handleSelectList = (id) => {
     setSelectListId(id);
-    axios.get(`${url}/lists/${id}/tasks`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      setTasks(res.data.tasks)
-    })
-    .catch((err) => {
-      setErrorMessage(`タスクの取得に失敗しました。${err}`);
-    })
-  }
+    axios
+      .get(`${url}/lists/${id}/tasks`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        setTasks(res.data.tasks);
+      })
+      .catch((err) => {
+        setErrorMessage(`タスクの取得に失敗しました。${err}`);
+      });
+  };
   return (
     <div>
       <Header />
@@ -70,22 +75,28 @@ export const Home = () => {
           <div className="list-header">
             <h2>リスト一覧</h2>
             <div className="list-menu">
-              <p><Link to="/list/new">リスト新規作成</Link></p>
-              <p><Link to={`/lists/${selectListId}/edit`}>選択中のリストを編集</Link></p>
+              <p>
+                <Link to="/list/new">リスト新規作成</Link>
+              </p>
+              <p>
+                <Link to={`/lists/${selectListId}/edit`}>
+                  選択中のリストを編集
+                </Link>
+              </p>
             </div>
           </div>
           <ul className="list-tab">
             {lists.map((list, key) => {
               const isActive = list.id === selectListId;
               return (
-                <li 
+                <li
                   key={key}
                   className={`list-tab-item ${isActive ? "active" : ""}`}
                   onClick={() => handleSelectList(list.id)}
                 >
                   {list.title}
                 </li>
-              )
+              );
             })}
           </ul>
           <div className="tasks">
@@ -94,55 +105,95 @@ export const Home = () => {
               <Link to="/task/new">タスク新規作成</Link>
             </div>
             <div className="display-select-wrapper">
-              <select onChange={handleIsDoneDisplayChange} className="display-select">
+              <select
+                onChange={handleIsDoneDisplayChange}
+                className="display-select"
+              >
                 <option value="todo">未完了</option>
                 <option value="done">完了</option>
               </select>
             </div>
-            <Tasks tasks={tasks} selectListId={selectListId} isDoneDisplay={isDoneDisplay} />
+            <Tasks
+              tasks={tasks}
+              selectListId={selectListId}
+              isDoneDisplay={isDoneDisplay}
+            />
           </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
 // 表示するタスク
 const Tasks = (props) => {
   const { tasks, selectListId, isDoneDisplay } = props;
-  if (tasks === null) return <></>
+  if (tasks === null) return <></>;
 
-  if(isDoneDisplay == "done"){
+  const diffSeconds = (task) => {
+    const now = new Date(); // 現在時刻を取得
+    const second = (new Date(task.limit) - now) / 1000;
+    const minutes = second / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+
+    const remainingDays = Math.floor(days);
+    const remainingHours = Math.floor(hours % 24); // 残りの時間
+    const remainingMinutes = Math.floor(minutes % 60); // 残りの分
+
+    return (
+        <>
+            <div className="timelimits">残り : {remainingDays}日{remainingHours}時間{remainingMinutes}分</div>
+        </>
+    );
+};
+  if (isDoneDisplay == "done") {
     return (
       <ul>
-        {tasks.filter((task) => {
-          return task.done === true
-        })
+        {tasks
+          .filter((task) => {
+            return task.done === true;
+          })
+          .sort((a, b) => new Date(a.limit) - new Date(b.limit))
+          .map((task, key) => (
+            <li key={key} className="task-item">
+              <Link
+                to={`/lists/${selectListId}/tasks/${task.id}`}
+                className="task-item-link"
+              >
+                <h1>{task.title}</h1>
+                <br />
+                <div>期日 : {moment(task.limit).format('MMMMDo HH:mm')}</div>
+              {diffSeconds(task)}
+                {task.done ? "完了" : "未完了"}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    );
+  }
+//未完了の処理 ↓
+
+return (
+    <ul>
+      {tasks
+        .filter((task) => task.done === false)
+        .sort((a, b) => new Date(a.limit) - new Date(b.limit))
         .map((task, key) => (
           <li key={key} className="task-item">
-            <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
-              {task.title}<br />
+            <Link
+              to={`/lists/${selectListId}/tasks/${task.id}`}
+              className="task-item-link"
+            >
+              <h1>{task.title}</h1>
+              <br />
+              <div>期日 : {moment(task.limit).format('MMMMDo HH:mm')}</div>
+              {diffSeconds(task)}
+              {/* <br /> */}
               {task.done ? "完了" : "未完了"}
             </Link>
           </li>
         ))}
-      </ul>
-    )
-  }
-
-  return (
-    <ul>
-      {tasks.filter((task) => {
-        return task.done === false
-      })
-      .map((task, key) => (
-        <li key={key} className="task-item">
-          <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
-            {task.title}<br />
-            {task.done ? "完了" : "未完了"}
-          </Link>
-        </li>
-      ))}
     </ul>
-  )
-}
+  );
+};
--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -3,17 +3,19 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import { url } from "../const";
 import { Header } from "../components/Header";
-import "./newTask.css"
-import { useHistory } from "react-router-dom";
-
+import "./newTask.scss";
+import { useNavigate } from "react-router-dom";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 export const NewTask = () => {
   const [selectListId, setSelectListId] = useState();
   const [lists, setLists] = useState([]);
   const [title, setTitle] = useState("");
   const [detail, setDetail] = useState("");
+  const [date, setDate] = useState(new Date());
   const [errorMessage, setErrorMessage] = useState("");
   const [cookies] = useCookies();
-  const history = useHistory();
+  const navigation = useNavigate();
   const handleTitleChange = (e) => setTitle(e.target.value);
   const handleDetailChange = (e) => setDetail(e.target.value);
   const handleSelectList = (id) => setSelectListId(id);
@@ -22,36 +24,46 @@ export const NewTask = () => {
       title: title,
       detail: detail,
       done: false,
+      limit: date,
     };
 
-    axios.post(`${url}/lists/${selectListId}/tasks`, data, {
+    axios
+      .post(`${url}/lists/${selectListId}/tasks`, data, {
         headers: {
-          authorization: `Bearer ${cookies.token}`
-        }
-    })
-    .then(() => {
-      history.push("/");
-    })
-    .catch((err) => {
-      setErrorMessage(`タスクの作成に失敗しました。${err}`);
-    })
-  }
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then(() => {
+        navigation("/");
+      })
+      .catch((err) => {
+        setErrorMessage(`タスクの作成に失敗しました。${err}`);
+      });
+  };
 
   useEffect(() => {
-    axios.get(`${url}/lists`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      setLists(res.data)
-      setSelectListId(res.data[0]?.id)
-    })
-    .catch((err) => {
-      setErrorMessage(`リストの取得に失敗しました。${err}`);
-    })
-  }, [])
+    axios
+      .get(`${url}/lists`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        setLists(res.data);
+        setSelectListId(res.data[0]?.id);
+      })
+      .catch((err) => {
+        setErrorMessage(`リストの取得に失敗しました。${err}`);
+      });
+  }, []);
 
+  const handleDate =(selectedDate) => {
+    const now = new Date();
+    if(selectedDate<now)
+    alert("選択された日付は現在時刻より前です。")
+    else
+        setDate(selectedDate);
+  }
   return (
     <div>
       <Header />
@@ -59,19 +71,64 @@ export const NewTask = () => {
         <h2>タスク新規作成</h2>
         <p className="error-message">{errorMessage}</p>
         <form className="new-task-form">
-          <label>リスト</label><br />
-          <select onChange={(e) => handleSelectList(e.target.value)} className="new-task-select-list">
+          <label>リスト</label>
+          <br />
+          <select
+            onChange={(e) => handleSelectList(e.target.value)}
+            className="new-task-select-list"
+          >
             {lists.map((list, key) => (
-              <option key={key} className="list-item" value={list.id}>{list.title}</option>
+              <option key={key} className="list-item" value={list.id}>
+                {list.title}
+              </option>
             ))}
-          </select><br />
-          <label>タイトル</label><br />
-          <input type="text" onChange={handleTitleChange} className="new-task-title" /><br />
-          <label>詳細</label><br />
-          <textarea type="text" onChange={handleDetailChange} className="new-task-detail" /><br />
-          <button type="button" className="new-task-button" onClick={onCreateTask}>作成</button>
+          </select>
+          <br />
+          <label>タイトル</label>
+          <br />
+          <input
+            type="text"
+            onChange={handleTitleChange}
+            className="new-task-title"
+          />
+          <br />
+          <label>期日</label>
+          <br />
+          <DatePicker
+        selected={date}
+        onChange={handleDate}
+        showTimeSelect
+        timeFormat="HH:mm"
+        timeIntervals={10}
+        timeCaption="time"
+        dateFormat="yyyy-MM-dd'T'HH:mm:ss'Z'"
+        className="new-task-date"
+      />
+          {/* <input 
+            type="datetime-local"
+            onChange={(e) => setDate(e.target.value)}
+            className="new-task-date"
+          /> */}
+          {console.log(date)}
+          <br />
+
+          <label>詳細</label>
+          <br />
+          <textarea
+            type="text"
+            onChange={handleDetailChange}
+            className="new-task-detail"
+          />
+          <br />
+          <button
+            type="button"
+            className="new-task-button"
+            onClick={onCreateTask}
+          >
+            作成
+          </button>
         </form>
       </main>
     </div>
-  )
-}
+  );
+};
